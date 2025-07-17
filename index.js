a7944baa9ea912956181f61d945ed5a9514d41cd
rafiqmia65 <rafiqdeveloper4465@gmail.com>
2025-07-18 00:59:17 +0600

    Final update

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ admin.initializeApp({
 async function run() {
   try {
     // Connect the client to the server (optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
 
     const db = client.db("mentorium");
     const usersCollection = db.collection("users");
@@ -1047,10 +1047,10 @@ async function run() {
     });
 
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log(
-      "Pinged your deployment. You successfully connected to MongoDB!"
-    );
+    // await client.db("admin").command({ ping: 1 });
+    // console.log(
+    //   "Pinged your deployment. You successfully connected to MongoDB!"
+    // );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
